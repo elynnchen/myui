a08c8defe7ae666d4fca41d69beaadb68561e3c6
elynnchen <44544738@qq.com>
2019-06-02 08:16:05 +0800

    插件完成

--- src/weui/Plugin.js
+++ src/weui/Plugin.js
@@ -1,10 +1,14 @@
 //写一个插件
- export default {
-     install(Vue, options) {
-         Vue.prototype.$toast = function (message) {
-             alert(message)
-         }
 
+import Toast from './WeToast'
+ export default {
+     install(Vue, options) {//Vue提供插件方法
+         Vue.prototype.$toast = function (message) {
+            let Constructor=Vue.extend(Toast);//构建实例
+             let toast= new Constructor();
+             toast.$mount();//将toast放到内存里
+             document.body.appendChild(toast.$el)//将Toast组件放到内面里
+         }
      }
  }
 
